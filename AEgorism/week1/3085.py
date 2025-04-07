a44e11c401901cb3108d05e406158041a7a2d56d
Yehun <yhyh4420@naver.com>
2025-04-07 09:42:31 +0900

    style: 주석 추가

--- AEgorism/week1/3085.py
+++ AEgorism/week1/3085.py
@@ -5,7 +5,7 @@ for _ in range(n):
     m.append(list(input()))
 
 answer = 1
-def max_count():
+def max_count(): # 맵이 주어졌을때 봄보니 최대값
     max_count = 1
     for i in range(n):
         garo_count = 1
@@ -25,15 +25,15 @@ def max_count():
             max_count = max(max_count, sero_count)
     return max_count
 
-answer = 1
-for i in range(n):
+answer = 1 # 무조건 하나는 먹으니까
+for i in range(n): # 위치를 바꾸는건 반복문으로 하되 그 안에서 최대값을 찾는건 함수로 뺀거임
     for j in range(n-1):
-        m[i][j], m[i][j+1] = m[i][j+1], m[i][j]
-        answer = max(answer, max_count())
-        m[i][j], m[i][j + 1] = m[i][j + 1], m[i][j]
-        m[j][i], m[j+1][i] = m[j+1][i], m[j][i]
-        answer = max(answer, max_count())
-        m[j][i], m[j + 1][i] = m[j + 1][i], m[j][i]
+        m[i][j], m[i][j+1] = m[i][j+1], m[i][j] # 가로 바꾸고
+        answer = max(answer, max_count()) # 비교한 다음
+        m[i][j], m[i][j + 1] = m[i][j + 1], m[i][j] # 원래대로
+        m[j][i], m[j+1][i] = m[j+1][i], m[j][i] # 다시 세로 바꾸고
+        answer = max(answer, max_count()) # 비교한 다음
+        m[j][i], m[j + 1][i] = m[j + 1][i], m[j][i] # 원래대로
 print(answer)
 
 # for i in range(n):
